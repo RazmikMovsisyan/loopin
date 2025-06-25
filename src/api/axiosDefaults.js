db1bb9efc42176fc3a909fd706d7b432b75e98e8
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-25 22:23:37 +0200

    Set Acces and Refresh Tokes for logged in Users

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://loopin-8006788e0f02.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
