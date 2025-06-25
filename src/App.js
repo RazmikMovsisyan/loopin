ed3a72f70b234ee71a2a7bfad330d021eb99f68c
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-25 15:44:28 +0200

    Tidy Up App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
-import SignInForm from './pages/auth/SignInForm'
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
