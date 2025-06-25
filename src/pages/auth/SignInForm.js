fb109bdd8ce13a20f87884a84d7bc1f4d8ea646b
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-25 15:27:18 +0200

    Configure and Style SignInForm

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -7,6 +9,7 @@ import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 function SignInForm() {
@@ -16,8 +19,33 @@ function SignInForm() {
       <Col className="my-auto p-0 p-md-2" md={10}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-          {/* Add your form here */}
+          <Form>
+                <Form.Group controlId="username">
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username"
+                        name="username" 
+                        className={styles.Input}
+                    />
+                </Form.Group>
 
+                <Form.Group controlId="password">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        name="password"
+                        className={styles.Input}
+                    />
+                </Form.Group>
+                <Button 
+                    className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                    type="submit"
+                >
+                    Sign In
+                </Button>
+            </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
