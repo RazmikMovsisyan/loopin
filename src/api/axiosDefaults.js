17410b5864d62bc54248cacc65cf7f80f87b8df7
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-26 13:00:05 +0200

    Cookie Fix

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 axios.defaults.baseURL = "https://loopin-8006788e0f02.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
@@ -7,6 +8,9 @@ axios.defaults.withCredentials = true;
 export const axiosReq = axios.create({
   baseURL: "https://loopin-8006788e0f02.herokuapp.com/",
   withCredentials: true,
+  headers: {
+    "X-CSRFToken": Cookies.get("csrftoken"),
+  },
 });
 
 export const axiosRes = axios.create({
