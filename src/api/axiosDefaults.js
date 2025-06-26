62cfde65643d2be9e34ba29a96971ee1462d5517
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-26 13:12:49 +0200

    403 Error Fix

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,19 +1,29 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-axios.defaults.baseURL = "https://loopin-8006788e0f02.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+const baseURL = "https://loopin-8006788e0f02.herokuapp.com/";
+
+axios.defaults.baseURL = baseURL;
 axios.defaults.withCredentials = true;
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 
 export const axiosReq = axios.create({
-  baseURL: "https://loopin-8006788e0f02.herokuapp.com/",
+  baseURL,
   withCredentials: true,
-  headers: {
-    "X-CSRFToken": Cookies.get("csrftoken"),
-  },
 });
 
 export const axiosRes = axios.create({
-  baseURL: "https://loopin-8006788e0f02.herokuapp.com/",
+  baseURL,
   withCredentials: true,
 });
+
+axiosReq.interceptors.request.use(
+  (config) => {
+    const csrfToken = Cookies.get("csrftoken");
+    if (csrfToken) {
+      config.headers["X-CSRFToken"] = csrfToken;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
