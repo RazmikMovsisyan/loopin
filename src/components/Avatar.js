622f4c7c021efd7c43025f83fe01f1639e836766
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-26 15:42:04 +0200

    Set user avatar across the app

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,17 +1,27 @@
 import React from "react";
-import styles from "../styles/Avatar.module.css";
+import { Image } from "react-bootstrap";
+import styles from "../styles/Avatar.module.css"; // falls vorhanden
 
-const Avatar = ({ src, height = 45, text }) => {
+const fallbackImage =
+  "https://res.cloudinary.com/dj5p9ubcu/image/upload/v1750632467/default_profile_rxsxdv.jpg";
+
+const Avatar = ({ src, alt = "avatar", height = 40, text = "", className = "" }) => {
+  const isFallback =
+    !src || src.includes("default_profile_rxsxdv") || src.startsWith("../");
+
+  const imageSrc = isFallback ? fallbackImage : src;
+
   return (
-    <span>
-      <img
-        className={styles.Avatar}
-        src={src}
+    <span className="d-inline-flex align-items-center">
+      <Image
+        src={imageSrc}
+        alt={alt}
         height={height}
         width={height}
-        alt="avatar"
+        roundedCircle
+        className={`me-1 ${className}`}
       />
-      {text}
+      {text && <strong>{text}</strong>}
     </span>
   );
 };
