79551922763a2386ea0f896c42b22aa264de0a11
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-09-01 16:48:43 +0200

    Axios Fix

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,31 +1,10 @@
-// src/api/axiosDefaults.js
 import axios from "axios";
-import Cookies from "js-cookie";
 
-const baseURL = "https://loopin-8006788e0f02.herokuapp.com/";
+// const baseURL = "https://loopin-8006788e0f02.herokuapp.com/";
 
-export const axiosReq = axios.create({
-  baseURL,
-  withCredentials: true,
-});
+axios.defaults.baseURL = "https://loopin-8006788e0f02.herokuapp.com";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
 
-export const axiosRes = axios.create({
-  baseURL,
-  withCredentials: true,
-});
-
-const methodsRequiringCSRF = ["post", "put", "patch", "delete"];
-
-axiosReq.interceptors.request.use(
-  (config) => {
-    const csrfToken = Cookies.get("csrftoken");
-    const method = config.method?.toLowerCase();
-
-    if (csrfToken && methodsRequiringCSRF.includes(method)) {
-      config.headers["X-CSRFToken"] = csrfToken;
-    }
-
-    return config;
-  },
-  (err) => Promise.reject(err)
-);
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
