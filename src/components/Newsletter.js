850fda755b712c17a3070f11527a4ffcb88db70f
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-09-13 23:50:32 +0200

    Fix: Unsubscribe button and remove E-Mail from Backend Admin

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -7,6 +7,7 @@ const Newsletter = () => {
   const [message, setMessage] = useState('');
   const [isSubscribed, setIsSubscribed] = useState(false);
   const [showUnsubscribe, setShowUnsubscribe] = useState(false);
+  const [subscribedEmail, setSubscribedEmail] = useState('');
 
   const handleSubscribe = async (e) => {
     e.preventDefault();
@@ -15,6 +16,7 @@ const Newsletter = () => {
       setMessage(response.data.message || 'Subscription successful');
       setIsSubscribed(true);
       setShowUnsubscribe(true);
+      setSubscribedEmail(email); // E-Mail für Unsubscribe speichern
       setEmail('');
     } catch (err) {
       setMessage(err.response?.data?.error || 'An error occurred. Please try again.');
@@ -22,16 +24,18 @@ const Newsletter = () => {
       if (email) {
         setShowUnsubscribe(true);
         setIsSubscribed(true);
+        setSubscribedEmail(email); // E-Mail für Unsubscribe speichern
       }
     }
   };
 
   const handleUnsubscribe = async () => {
     try {
-      await axiosReq.post('/newsletter/unsubscribe_by_email/', { email });
+      await axiosReq.post('/newsletter/unsubscribe_by_email/', { email: subscribedEmail });
       setMessage('You have been successfully unsubscribed from our newsletter.');
       setIsSubscribed(false);
       setShowUnsubscribe(false);
+      setSubscribedEmail('');
     } catch (err) {
       setMessage(err.response?.data?.error || 'An error occurred while unsubscribing. Please try again.');
     }
@@ -40,6 +44,7 @@ const Newsletter = () => {
   return (
     <div className={styles.Newsletter}>
       <h5>Subscribe to our Newsletter</h5>
+      
       {!isSubscribed ? (
         <form onSubmit={handleSubscribe}>
           <input
