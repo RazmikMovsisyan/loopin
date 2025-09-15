ea82c0d03c05fd087772d8f78407edff688f3c59
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-09-15 20:07:32 +0200

    Mobile: Login Issue Fix Attempt

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const local = false;
 
 axios.defaults.baseURL = local ? "http://localhost:8000" : "https://loopin-8006788e0f02.herokuapp.com";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
+// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
