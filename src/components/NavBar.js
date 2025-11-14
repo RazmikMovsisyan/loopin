aa987c05ce516ca2fdb91af62008a3b86a1734b9
Razmik Movsisyan <razmikmovsisyan@icloud.com>
2025-11-15 00:20:26 +0100

    Feat: Enhance logo for better performance

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -143,7 +143,7 @@ const NavBar = () => {
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="45" />
+            <img src={logo} alt="logo" height="60" />
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
